b05a09ea59f1299334ad46b84d1fde06ba8119b9
Leonardo da Silva <silvaleonardo.ti@gmail.com>
2022-02-24 17:47:17 -0300

    cleaning old code,

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,18 @@
 import React, { useEffect, useState } from "react";
-import {
-  Button,
-  Center,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-  useToast,
-  Wrap,
-  WrapItem,
-} from "@chakra-ui/react";
+import { Button, VStack } from "@chakra-ui/react";
 import { Pomodoro } from "./Pomodoro";
 
 const TOTAL_SECONDS_AMOUNT = 15 * 60 - 895;
 
 export const Home: React.FC = () => {
-  const toast = useToast();
   const [secondsAmount, setSecondsAmount] = useState(TOTAL_SECONDS_AMOUNT);
   const [isCounting, setIsCounting] = useState(false);
-
-  // const [currentPomoIsDone, setCurrentPomoIsDone] = useState(false);
-  // const [type, setType] = useState("work" || "short-break" || "long-break");
 
   useEffect(() => {
     if (secondsAmount > 0 && isCounting) {
       setTimeout(() => {
         setSecondsAmount((old) => old - 1);
       }, 1000);
-    } else {
-      toast({
-        title: "Your pomo is done!",
-        status: "success",
-        duration: 5000,
-        isClosable: true,
-      });
-      setIsCounting(false);
     }
   }, [isCounting, secondsAmount]);
 
@@ -44,44 +21,12 @@ export const Home: React.FC = () => {
   };
 
   return (
-    <Wrap>
-      <WrapItem>
-        <Center>
-          {/* <Tabs align="center" variant="soft-rounded" colorScheme="blue">
-            <TabList>
-              <Tab>Work</Tab>
-              <Tab>Short Break</Tab>
-              <Tab>Long Break</Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel> */}
-          <Pomodoro
-            secondsAmount={secondsAmount}
-            type={"work"}
-            // defineAsDone={setCurrentPomoIsDone}
-          />
-          <Button onClick={() => handleStartStop()}>
-            {isCounting ? "Pausar" : "Iniciar"}
-          </Button>
-          {/* </TabPanel>
-              <TabPanel>
-                <Pomodoro
-                  secondsAmount={secondsAmount}
-                  type={"work"}
-                  // defineAsDone={setCurrentPomoIsDone}
-                />
-              </TabPanel>
-              <TabPanel>
-                <Pomodoro
-                  secondsAmount={secondsAmount}
-                  type={"work"}
-                  // defineAsDone={setCurrentPomoIsDone}
-                />
-              </TabPanel>
-            </TabPanels>
-          </Tabs> */}
-        </Center>
-      </WrapItem>
-    </Wrap>
+    <VStack>
+      <Pomodoro secondsAmount={secondsAmount} message={"work"} />
+
+      <Button onClick={() => handleStartStop()}>
+        {isCounting ? "Pausar" : "Iniciar"}
+      </Button>
+    </VStack>
   );
 };
--- src/pages/Pomodoro.tsx
+++ src/pages/Pomodoro.tsx
@@ -1,76 +1,24 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React from "react";
 import {
   Box,
-  Button,
   CircularProgress,
   CircularProgressLabel,
   VStack,
 } from "@chakra-ui/react";
-import moment from "moment";
-import { clearTimeout, setTimeout } from "timers";
-// import { ArrowRightIcon } from "@chakra-ui/icons";
 
 interface IPomodoro {
   secondsAmount: number;
-  type: string;
-  defineAsDone?: (value: boolean) => void;
+  message?: string;
 }
 
-export const Pomodoro: React.FC<IPomodoro> = ({
-  secondsAmount,
-  type,
-  defineAsDone,
-}) => {
-  // const [timeInSeconds, setTimeInSeconds] = useState(minutes * 60);
-  // const [percentualDecorrido, setPercentualDecorrido] = useState(0);
-  // const formatted = moment.utc(timeInSeconds * 1000).format("mm:ss");
+export const Pomodoro: React.FC<IPomodoro> = ({ secondsAmount, message }) => {
   const minutes = Math.floor(secondsAmount / 60);
   const seconds = secondsAmount % 60;
-
-  // const percentualPorTempo = useMemo(() => {
-  //   // return 100 / (secondsAmount * 60);
-  // }, [secondsAmount]);
-
-  const message = useMemo(() => {
-    switch (type) {
-      case "work":
-        return "Work!";
-      case "short-break":
-        return "short-break!";
-      case "long-break":
-        return "long-break!";
-    }
-  }, [type]);
-
-  // useEffect(() => {
-  //   if (isCounting) {
-  //     const time = setTimeout(() => {
-  //       setTimeInSeconds(timeInSeconds - 1);
-  //       setPercentualDecorrido(percentualDecorrido + percentualPorTempo);
-  //     }, 1000);
-  //     if (timeInSeconds === 0) {
-  //       clearTimeout(time);
-  //       defineAsDone(true);
-  //     }
-  //   }
-  // }, [
-  //   defineAsDone,
-  //   isCounting,
-  //   percentualDecorrido,
-  //   percentualPorTempo,
-  //   timeInSeconds,
-  // ]);
-
-  // const resetPomo = useCallback(() => {
-  //   setIsCounting(false);
-  //   setTimeInSeconds(minutes * 60);
-  //   setPercentualDecorrido(0);
-  // }, [minutes]);
 
   return (
     <VStack>
       <Box>
-        <CircularProgress value={1} size="500px" thickness="1px" capIsRound>
+        <CircularProgress value={0} size="500px" thickness="1px" capIsRound>
           <CircularProgressLabel>{`${minutes
             .toString()
             .padStart(2, "0")}:${seconds
@@ -81,14 +29,6 @@ export const Pomodoro: React.FC<IPomodoro> = ({
 
       <Box fontWeight={"bold"} fontSize={18} fontFamily={"Helvetica"}>
         {message}
-      </Box>
-
-      <Box>
-        {/* {isCounting && (
-          <Button onClick={() => resetPomo()}>
-            <ArrowRightIcon />
-          </Button>
-        )} */}
       </Box>
     </VStack>
   );
