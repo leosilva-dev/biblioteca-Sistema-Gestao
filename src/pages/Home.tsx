41b46655a578123574dc5c6c856d0c19083535ea
Leonardo da Silva <silvaleonardo.ti@gmail.com>
2022-02-26 01:54:12 -0300

    Alterando layout do app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { HStack, VStack } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
 import { Pomodoro } from "../shared/components/Pomodoro/Pomodoro";
 import { TaskList } from "../shared/components/task/TaskList";
 
 export const Home: React.FC = () => {
   return (
-    <HStack>
-      <VStack>
-        <TaskList />
-      </VStack>
-      <VStack>
-        <Pomodoro />
-      </VStack>
-    </HStack>
+    <VStack>
+      <Pomodoro />
+      <TaskList />
+    </VStack>
   );
 };
