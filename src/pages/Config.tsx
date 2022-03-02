65e1f84a2b216f292f827891bfc7192f59f96c76
Leonardo da Silva <silvaleonardo.ti@gmail.com>
2022-03-02 15:34:08 -0300

    Use toggle dark mode on config page

--- src/pages/Config.tsx
+++ src/pages/Config.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react";
 import { Text, VStack } from "@chakra-ui/react";
+import { ToggleMode } from "../shared/components/toggleMode/ToggleMode";
 
 export const Config: React.FC = () => {
   useEffect(() => {
     document.title = "PomoTask - Configurantion";
   }, []);
   return (
-    <VStack>
+    <VStack padding={2}>
       <Text fontSize="2xl">Config page</Text>
+      <ToggleMode />
     </VStack>
   );
 };
